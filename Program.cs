e26eecb58802f03e9417f8295bd6d98fecf7ed06
Simeon Bukurov <71982116+SwagerStorm@users.noreply.github.com>
2020-09-27 22:06:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmdArgs = command.Split();
+            var cmdType = cmdArgs[0];
+            switch (cmdType)
+            {
+                case "Create":
+                    Create(cmdArgs, accounts);
+                    break;
+                case "Deposit":
+                    Deposit(cmdArgs, accounts);
+                    break;
+                case "Withdraw":
+                    Withdraw(cmdArgs, accounts);
+                    break;
+                case "Print":
+                    Print(cmdArgs, accounts);
+                    break;
+            }
+        }
+    }
+}
